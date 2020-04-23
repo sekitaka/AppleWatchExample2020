464448884696ab843e257a85d7ef39d7f9e8c09c
sekitaka-RBS <netstaff12@runners.co.jp>
2020-04-23 09:25:39 +0900

    Initial Commit

---
+++ WatchAppExample WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchAppExample WatchKit Extension
+//
+//  Created by Takashi Seki on 2020/04/23.
+//  Copyright © 2020 Takashi Seki. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ WatchAppExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchAppExample
+//
+//  Created by Takashi Seki on 2020/04/23.
+//  Copyright © 2020 Takashi Seki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
